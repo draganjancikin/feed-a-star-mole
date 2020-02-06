d19d39dda7b46b904913123249d60cbef3829f82
Dragan Jancikin <dragan.jancikin@gmail.com>
2020-02-06 16:48:42 +0100

    some code optimization

--- js/script.js
+++ js/script.js
@@ -4,61 +4,61 @@
 const moles = [
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-0")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-1")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-2")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-3")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-4")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-5")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-6")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-7")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-8")
   },
   {
     state: "sad",
-    timeToNext: getSadTime(),
+    timeToNext: getTimeToNext("sad"),
     king: false,
     node: document.querySelector("#hole-9")
   }
@@ -108,87 +108,98 @@ function getNextStatus (mole) {
   switch (mole.state) {
     case "sad":
     case "fed":
-      mole.state = "leaving";
-      mole.timeToNext = getLeavingTime();
-      if(mole.king) {
-        mole.node.firstElementChild.src = "images/king-mole-leaving.png";  
-      } else {
-        mole.node.firstElementChild.src = "images/mole-leaving.png";
-      } 
-      if (game.score >= game.WIN_SCORE) {
-        bgGame.classList.add("hiden");
-        bgGameOver.classList.remove("hiden");
-        game.isFinish = true;
-      }
+      getLeavingState(mole);
       break;
     case "leaving":
-      mole.state = "gone";
-      mole.timeToNext = getGoneTime();
-      mole.king = false;
-      mole.node.firstElementChild.classList.add("gone");
+      getGoneState(mole);
       break;
     case "gone":
-      mole.state = "hungry";
-      mole.timeToNext = getHungryTime();
-      mole.king = getKing();
-      if(mole.king) {
-        mole.node.firstElementChild.src = "images/king-mole-hungry.png";  
-      } else {
-        mole.node.firstElementChild.src = "images/mole-hungry.png";
-      }
-      mole.node.firstElementChild.classList.remove("gone");
-      mole.node.firstElementChild.classList.add("hungry");
+      getHungryState(mole);
       break;
     case "hungry":
-      mole.state = "sad";
-      mole.timeToNext = getSadTime();
-      if(mole.king) {
-        mole.node.firstElementChild.src = "images/king-mole-sad.png";  
-      } else {
-        mole.node.firstElementChild.src = "images/mole-sad.png";
-      }
-      mole.node.firstElementChild.classList.remove("hungry");
+      getSadState(mole);
       break;
   }
 }
 
-// get interval for leaving mole .....................................
-function getLeavingTime () {
-  return Date.now() + 500;
+// get mole with state leaving .......................................
+function getLeavingState (mole) {
+  mole.state = "leaving";
+  mole.timeToNext = getTimeToNext(mole.state);
+  if(mole.king) {
+    mole.node.firstElementChild.src = "images/king-mole-leaving.png";  
+  } else {
+    mole.node.firstElementChild.src = "images/mole-leaving.png";
+  } 
+  if (game.score >= game.WIN_SCORE) {
+    bgGame.classList.add("hiden");
+    bgGameOver.classList.remove("hiden");
+    game.isFinish = true;
+  }
 }
 
-// get empty hole interval ...........................................
-function getGoneTime () {
-  // between 2 and 18 sec
-  return Date.now() + (Math.floor(Math.random() * 18000) + 2000);
+// get mole with state gone ..........................................
+function getGoneState (mole) {
+  mole.state = "gone";
+  mole.timeToNext = getTimeToNext(mole.state);
+  mole.king = false;
+  mole.node.firstElementChild.classList.add("gone");
 }
 
-// get interval for hungry mole ......................................
-function getHungryTime () {
-  return Date.now() + 2000;
+// get mole with state hungry ........................................
+function getHungryState (mole) {
+  mole.state = "hungry";
+  mole.timeToNext = getTimeToNext(mole.state);
+  mole.king = getKing();
+  if(mole.king) {
+    mole.node.firstElementChild.src = "images/king-mole-hungry.png";  
+  } else {
+    mole.node.firstElementChild.src = "images/mole-hungry.png";
+  }
+  mole.node.firstElementChild.classList.remove("gone");
+  mole.node.firstElementChild.classList.add("hungry");
 }
 
-// get interval for sad mole .........................................
-function getSadTime () {
-  return Date.now() + 2000;
+// get mole with state sad ...........................................
+function getSadState(mole) {
+  mole.state = "sad";
+  mole.timeToNext = getTimeToNext(mole.state);
+  if(mole.king) {
+    mole.node.firstElementChild.src = "images/king-mole-sad.png";  
+  } else {
+    mole.node.firstElementChild.src = "images/mole-sad.png";
+  }
+  mole.node.firstElementChild.classList.remove("hungry");
 }
 
-// get interval for fed...............................................
-function getFedTime () {
-  return Date.now() + 500;
+// get time to next state ...........................................
+function getTimeToNext (state) {
+  switch(state){
+    case "leaving":
+    case "fed":
+      return Date.now() + 500;
+      break;
+    case "gone":
+      return Date.now() + (Math.floor(Math.random() * 18000) + 2000);
+      break;
+    case "hungry":
+    case "sad":
+      return Date.now() + 2000;
+      break;
+  }
 }
 
 // get king ..........................................................
 function getKing(){
   return Math.random() >= 0.9;
-}
+} 
 
 // Event Listener ====================================================
 bgGame.addEventListener("click", function(event){
   if (event.target.classList.value.includes("hungry")) {
     let index = event.target.dataset.index;
     moles[index].state = "fed";
-    moles[index].timeToNext = getFedTime();
+    moles[index].timeToNext = getTimeToNext(moles[index].state);
     if(moles[index].king) {
       moles[index].node.firstElementChild.src = "images/king-mole-fed.png";
       game.score +=2;  
